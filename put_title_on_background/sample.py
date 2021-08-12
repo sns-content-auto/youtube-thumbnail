d016c7985b030510cc33f040d492f0d0f9f1fb60
nobu513 <63525140+nobu513@users.noreply.github.com>
2021-08-12 19:03:16 +0900

    delete some files

--- put_title_on_background/sample.py
+++
@@ -1,4 +0,0 @@
-fruits = ["orange", "mikan", "daikon"]
-fruits_next = fruits[:-1]
-msg = "helloworld\n"
-print(msg[:-1])
--- put_title_on_background/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-cv.rectangle(img, (0, 0), (wrap_width, wrap_height), (0, 255, 0), thickness=-1)
-
-
-cv.imshow("img", img)
-cv.waitKey()
